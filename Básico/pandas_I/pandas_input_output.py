5933b287d85c1249256f0c8ab3345cf8df41158e
Jaime Gonzalez <68569736+jaime-gonzalez@users.noreply.github.com>
2020-11-21 19:21:55 -0300

    Create pandas_input_output.py

---
+++ Básico/pandas_I/pandas_input_output.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv('C:/Users/55479/Desktop/Pandas/iris.data')
+
+df.head()
+
+df.dtypes
+
+df.shape
+
+df.describe()
+
+df.to_excel('C:/Users/55479/Desktop/Pandas/iris.xlsx')
